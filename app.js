79098b4aa2b049f18ad90eaa895a91c87055f2ed
cerickson21 <45769406+cerickson21@users.noreply.github.com>
2020-07-31 15:44:59 -0600

    Revert "button exists (map doesn't)"
    This reverts commit 48393c7764d9ba58842ca4f5d76a8df4422d6cef.

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ var map, infoWindow;
 var region = "Region 0";
 var prevLat = 0;
 var prevLong = 0;
-var currLat = 0;
-var currLong = 0;
-var distanceTravelled = 0; //should be in miles
-var score = distanceTravelled * 100;
 
 //Radar work
 Radar.initialize("prj_test_pk_c3a8e42cc392005ceac13fb535867c684ee1b208");
@@ -56,15 +52,4 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
                         'Error: The Geolocation service failed.' :
                         'Error: Your browser doesn\'t support geolocation.');
   infoWindow.open(map);
-}
-
-function calcDistanceTravelled(lat1, lat2, long1, long2){
-  var lat1Radians = lat1/(180/Math.PI);
-  var lat2Radians = lat2/(180/Math.PI);
-  var long1Radians = long1/(180/Math.PI);
-  var long2Radians = long2/(180/Math.PI);
-
-  a = (Math.sin(lat1) * Math.sin(lat2)) + Math.cos(lat1) * Math.cos(lat2) *
-    Math.cos(long2 – long1))
-  distanceTravelled = 3963.0 * Math.arccos(a)
 }
