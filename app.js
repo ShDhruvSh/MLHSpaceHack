4a5e9887ba8adc7762802fcd2af09f9cad00b6e3
Emily Amspoker <eamspoker21@kentdenver.org>
2020-07-31 18:23:50 -0600

    changed location of js

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 var map, infoWindow;
 var region = "Region 0";
-var prevLat = position.coords.latitude;
-var prevLong = position.coords.longitude;
-var currLat = position.coords.latitude;
-var currLong = position.coords.longitude;
-var distanceTravelled = calcDistanceTravelled(prevLat, currLat, prevLong, currLong);
-var score = score + (distanceTravelled * 100);
+
 
 //Radar work
 Radar.initialize("prj_test_pk_c3a8e42cc392005ceac13fb535867c684ee1b208");
@@ -56,6 +51,13 @@ function initMap() {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
+
+      var prevLat = position.coords.latitude;
+      var prevLong = position.coords.longitude;
+      var currLat = position.coords.latitude;
+      var currLong = position.coords.longitude;
+      var distanceTravelled = calcDistanceTravelled(prevLat, currLat, prevLong, currLong);
+      var score = score + (distanceTravelled * 100);
 
       infoWindow.setPosition(pos);
       infoWindow.setContent(region);
